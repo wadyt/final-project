f26e8128cd53985ce2c9db66b93b52ccdf267225
wadyt <wady.tupiza@gmail.com>
2017-07-15 19:09:48 -0500

    final project

--- public/js/db.js
+++ public/js/db.js
@@ -4,7 +4,7 @@ var config = {
   user: 'wady', //env var: PGUSER
   database: 'recipebookdb', //env var: PGDATABASE
   password: '1234', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
+  host: 'postgres://evmadugeuvpyii:b918c11f638ad50cac5fc912693a8783ff24f125c871b1c8bd080bb35884e2a2@ec2-107-21-99-176.compute-1.amazonaws.com:5432/devj46kojun2ua', // Server hosting the postgres database
   port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
